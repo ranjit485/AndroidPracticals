066144b1e918f42f40a525d8100171559314b792
ranjit485 <patilranjit485@gmail.com>
2024-01-29 14:57:17 +0530

    first commit

---
+++ app/src/main/java/com/example/practicalmad/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.practicalmad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pr_5_e_2);
+    }
+}
