e3f72dd1e1e5ed6603ce1b1464df39bbc4b0b3ef
Administrator <patilranjit485@gmail.com>
2024-01-30 11:10:29 +0530

    login page design ADDED

--- app/src/main/java/com/example/practicalmad/MainActivity.java
+++ app/src/main/java/com/example/practicalmad/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pr_5_e_2);
+        setContentView(R.layout.login1);
     }
 }
