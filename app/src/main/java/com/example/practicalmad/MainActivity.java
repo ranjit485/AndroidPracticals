a15b87dad7d068022ee7ad8e10b55673cb9dc035
ranjit485 <patilranjit485@gmail.com>
2024-03-02 16:54:40 +0530

    new update 6 7 e1 e2

--- app/src/main/java/com/example/practicalmad/MainActivity.java
+++ app/src/main/java/com/example/practicalmad/MainActivity.java
@@ -1,34 +1,57 @@
 package com.example.practicalmad;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton radioButton1_1;
+    private RadioButton radioButton1_2;
+    private RadioGroup radioGroup2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pr_7_e2);
-        Button btn = findViewById(R.id.btn);
-        EditText name = findViewById(R.id.name);
-        EditText age = findViewById(R.id.age);
-        EditText roll = findViewById(R.id.roll);
-        TextView dname = findViewById(R.id.dname);
-        TextView dage = findViewById(R.id.dage);
-        TextView droll = findViewById(R.id.droll);
+        setContentView(R.layout.pr_12_e1);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dname.setText("Name : "+name.getText());
-                dage.setText("Age : "+age.getText());
-                droll.setText("Roll : "+roll.getText());
-            }
-        });
+        radioButton1_1 = findViewById(R.id.radioButton1_1);
+        radioButton1_2 = findViewById(R.id.radioButton1_2);
+        radioGroup2 = findViewById(R.id.radioGroup2);
+    }
+
+    public void showSelection(View view) {
+        String selectedOption1 = getSelectedOption(radioButton1_1, radioButton1_2);
+        String selectedOption2 = getSelectedOptionFromGroup(radioGroup2);
+
+        String message = "Selected Options: " + selectedOption1 + ", " + selectedOption2;
+        showToast(message);
+    }
+
+    private String getSelectedOption(RadioButton radioButton1, RadioButton radioButton2) {
+        if (radioButton1.isChecked()) {
+            return radioButton1.getText().toString();
+        } else if (radioButton2.isChecked()) {
+            return radioButton2.getText().toString();
+        } else {
+            return "None";
+        }
+    }
+
+    private String getSelectedOptionFromGroup(RadioGroup radioGroup) {
+        int selectedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+
+        if (selectedRadioButtonId != -1) {
+            RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
+            return selectedRadioButton.getText().toString();
+        } else {
+            return "None";
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
